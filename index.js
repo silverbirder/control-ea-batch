cd0d40ee1de9d2c10f36ffa6c40509d4e7aa9ed3
shibamoto <silverbirder@gmail.com>
2019-01-31 23:07:06 +0900

    add basic response data

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer');
+const node_html_parser  = require('node-html-parser');
 
-exports.getVipData = async (req, res) => {
+const target_host = 'https://www.fxstreet.jp';
+const target_path = 'economic-calendar';
+const detail_selector = "#fxst-calendartable > tbody > tr.fxit-eventrow > td > div";
+
+getVipData = async (req, res) => {
     const browser = await puppeteer.launch({
         args: [
             '--no-sandbox',
@@ -8,10 +13,41 @@ exports.getVipData = async (req, res) => {
         ]
     });
     const page = await browser.newPage();
-    await page.goto('https://www.google.com/', {waitUntil: 'networkidle2'});
-    var data = await page.evaluate((selector) => {
-        return document.querySelector(selector).textContent;
-    }, "body");
+    // dummy access to avoid advertising
+    await page.goto(target_host + '/' + target_path, {waitUntil: 'networkidle2'});
+
+    await page.reload();
+    // wait for the search box to appear
+    await page.waitFor(5000);
+
+    // extract html
+    const innerDataList = await page.evaluate((selector) => {
+        const list = Array.from(document.querySelectorAll(selector));
+        return list.map(data => {
+            return data.innerHTML;
+        });
+    }, detail_selector);
+
+    // format to desired data
+    const parsedDataList = innerDataList.map(value => {
+       const parsedHtml = node_html_parser.parse(value);
+       const dateHtml = parsedHtml.querySelector('.fxit-eventInfo-time').removeWhitespace().toString();
+       const currencyHtml = parsedHtml.querySelector('.fxit-event-name').removeWhitespace().toString();
+       const titleHtml = parsedHtml.querySelector('.fxit-event-title').removeWhitespace().toString();
+       const volatilityHtml = parsedHtml.querySelector('.fxit-eventInfo-vol-c').removeWhitespace().toString();
+       return {
+           date: node_html_parser.parse(dateHtml).text,
+           currency: node_html_parser.parse(currencyHtml).text,
+           title: node_html_parser.parse(titleHtml).text,
+           volatility: node_html_parser.parse(volatilityHtml).text
+       }
+    });
+
+    // save
+    // ...
     browser.close();
-    res.send(data);
+
+    res.send(parsedDataList)
 };
+
+exports.getVipData = getVipData;
